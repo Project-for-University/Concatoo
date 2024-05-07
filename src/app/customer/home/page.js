7cc8d84733796809c8c31c5c272b7da02b75bc09
aghniihsn <aghniihsn.gmail.com>
2024-05-07 13:18:23 +0700

    form fe login & register

--- src/app/customer/home/page.js
+++ src/app/customer/home/page.js
@@ -1,7 +1,3 @@
 export default function Pages() {
-    return (
-        <>
-            <h1>home user</h1>
-        </>
-    );
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +17,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
+
+  theme: {},
 };
