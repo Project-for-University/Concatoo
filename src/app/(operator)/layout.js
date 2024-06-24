812d4a8c8f59742f862b9a39fc9e24f904c4e61d
aghniihsn <aghniihsn.gmail.com>
2024-06-24 20:37:39 +0700

    footer about di seller dan operator

--- src/app/(operator)/layout.js
+++ src/app/(operator)/layout.js
@@ -119,16 +119,7 @@ export default function RootLayout({ children }) {
                                 </Link>
                                 <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                                     <li>
-                                        <a href="#" className="hover:underline me-4 md:me-6">About</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:underline">Contact</a>
+                                        <Link href="/about" className="hover:underline me-4 md:me-6">About</Link>
                                     </li>
                                 </ul>
                             </div>
